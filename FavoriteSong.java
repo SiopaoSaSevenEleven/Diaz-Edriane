ee31fbf0a3d08cec75efcfad3f0546eea0e67d33
Pop <126241480+SiopaoSaSevenEleven@users.noreply.github.com>
2023-10-17 02:51:20 +0100

    Add files via upload

---
+++ FavoriteSong.java
@@ -0,0 +1,10 @@
+public class FavoriteSong{
+   public static void main(String[]args){
+   
+   System.out.println("I can't lose when I'm with you");
+  System.out.println("How can I snooze and miss the moment?");
+  System.out.println("You just too important");
+  System.out.println("Nobody do body like you do");
+ 
+   }
+}
